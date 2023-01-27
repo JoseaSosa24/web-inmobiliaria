59f391736203d0686731f132fcba4912fbed697b
JOSE SOSA <josea_1998@hotmail.com>
2023-01-27 17:52:00 -0500

    Se crea componete titulo

---
+++ front/src/helpers/Titulo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Titulo = ({children}) => {
+    return (
+        <section className='section-titulo'>
+            <h2 className='titulo'> {children} </h2>
+        </section>
+    )
+}
