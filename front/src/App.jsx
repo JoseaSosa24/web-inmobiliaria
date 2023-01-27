1d4de5da415b05aa445a7635350508fbab51e1fd
JOSE SOSA <josea_1998@hotmail.com>
2023-01-27 18:35:17 -0500

    Se insertan componentes

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,12 +1,20 @@
 import { useState } from 'react'
 import './App.css'
+import Menu from './helpers/Menu/Menu'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Inicio from './helpers/Inicio/Inicio'
+import Nosotros from './helpers/Nosotros/Nosotros'
+import { Anuncios } from './helpers/Anuncios/Anuncios'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      
+      <Menu />
+      <Inicio/>
+      <Nosotros/>
+      <Anuncios/>
     </div>
   )
 }
