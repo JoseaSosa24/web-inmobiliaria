57daa9c4b7a8cc2dcf6040ea31a682b2ab1ce093
JOSE SOSA <josea_1998@hotmail.com>
2023-01-26 23:49:41 -0500

    Se crea componente inicio que contiene imagen

---
+++ front/src/helpers/Inicio/Inicio.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Carousel } from 'bootstrap';
+import './Inicio.css'
+const Inicio = () => {
+    return (
+        <section className='inicio' id='inicio'>
+
+            <img class="" src="../../../src/assets/img/inicio.jpg" >
+                
+            </img>
+        </section>
+    );
+}
+
+export default Inicio;
