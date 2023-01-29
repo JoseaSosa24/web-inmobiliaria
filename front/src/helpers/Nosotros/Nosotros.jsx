b6e681c1c4576c182c0e6519dd73ee589891c3f1
JOSE SOSA <josea_1998@hotmail.com>
2023-01-28 23:39:50 -0500

    Se cambia color de la letra y fondo

--- front/src/helpers/Nosotros/Nosotros.jsx
+++ front/src/helpers/Nosotros/Nosotros.jsx
@@ -26,8 +26,8 @@ const Nosotros = () => {
   }, []);
 
   return (
-    <section className="nosotros d-flex flex-column align-items-center justify-content-center" id="nosotros">
-      <h1>Nosotros</h1>
+    <section className="nosotros d-flex flex-column align-items-center justify-content-center" id="nosotros" >
+      <h3>Nosotros</h3>
       <p className="text-center w-75">
         Somos InmoWeb, una empresa líder en el mercado inmobiliario. Ofrecemos una amplia variedad de servicios, desde la compra y venta de propiedades hasta la gestión de alquileres. Nuestro equipo está compuesto por profesionales altamente capacitados y con una amplia experiencia en el sector inmobiliario. Trabajamos con dedicación y pasión para brindarles a nuestros clientes la mejor experiencia en sus transacciones inmobiliarias.
       </p>
