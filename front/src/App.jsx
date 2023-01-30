fadc7eef5818f5d71a88876713cdb824b7c7106b
JOSE SOSA <josea_1998@hotmail.com>
2023-01-30 02:14:55 -0500

    se agrega componente footer

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -7,6 +7,7 @@ import Nosotros from './helpers/Nosotros/Nosotros'
 import { Anuncios } from './helpers/Anuncios/Anuncios'
 import { Servicios } from './helpers/Servicios/Servicios'
 import { Contacto } from './helpers/Contacto/Contacto'
+import { Footer } from './helpers/Footer/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -19,6 +20,7 @@ function App() {
       <Anuncios />
       <Servicios />
       <Contacto />
+      <Footer />
     </div>
   )
 }
