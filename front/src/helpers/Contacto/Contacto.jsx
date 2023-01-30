b112cb0eb7a1d7ef614f6760993728bea907f335
JOSE SOSA <josea_1998@hotmail.com>
2023-01-30 02:16:05 -0500

    se hacen modificaciones a las ección contacto

--- front/src/helpers/Contacto/Contacto.jsx
+++ front/src/helpers/Contacto/Contacto.jsx
@@ -3,9 +3,12 @@ import './Contacto.css'
 export const Contacto = () => {
     return (
         <section className='contacto' id='contacto'>
+
             <div class="form-container" id=''>
+            
+                <h2 class="form-title">contacto</h2>
                 <form class="form">
-                    <h2 class="form-title">Formulario de contacto</h2>
+                <h6 className='mt-5 mb-5 form-leyenda'>Nuestro negocio no es vender propiedades, sino ayudar a nuestro cliente a tomar la decisión perfecta.</h6>
                     <div class="form-group">
                         <input type="text" placeholder="Nombre" required />
                     </div>
@@ -16,7 +19,7 @@ export const Contacto = () => {
                         <input type="tel" placeholder="Celular" required />
                     </div>
                     <div class="form-group">
-                        <textarea placeholder="Motivo de la solicitud"  required></textarea>
+                        <textarea placeholder="Motivo de la solicitud" required></textarea>
                     </div>
 
                     <button type="submit">Enviar</button>
