6bbbde244a54ab8e7215e3da6403e5cd3a7b959f
JOSE SOSA <josea_1998@hotmail.com>
2023-01-30 16:53:53 -0500

    Se edita componenten contacto

--- front/src/helpers/Contacto/Contacto.jsx
+++ front/src/helpers/Contacto/Contacto.jsx
@@ -4,21 +4,21 @@ export const Contacto = () => {
     return (
         <section className='contacto' id='contacto'>
 
-            <div class="form-container" id=''>
-            
-                <h2 class="form-title">contacto</h2>
-                <form class="form">
-                <h6 className='mt-5 mb-5 form-leyenda'>Nuestro negocio no es vender propiedades, sino ayudar a nuestro cliente a tomar la decisión perfecta.</h6>
-                    <div class="form-group">
+            <div className="form-container" id=''>
+
+                <h2 className="form-title">contacto</h2>
+                <form className="form">
+                    <p classNameName="form-parrafo" id='form-parrafo'>Nuestro negocio no es vender propiedades, sino ayudar a nuestro cliente a tomar la decisión perfecta.</p>
+                    <div className="form-group">
                         <input type="text" placeholder="Nombre" required />
                     </div>
-                    <div class="form-group">
+                    <div className="form-group">
                         <input type="email" placeholder="Correo" required />
                     </div>
-                    <div class="form-group">
+                    <div className="form-group">
                         <input type="tel" placeholder="Celular" required />
                     </div>
-                    <div class="form-group">
+                    <div className="form-group">
                         <textarea placeholder="Motivo de la solicitud" required></textarea>
                     </div>
 
