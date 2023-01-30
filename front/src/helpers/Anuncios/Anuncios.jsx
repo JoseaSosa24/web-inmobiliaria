ec7707d8fe22d1d17282c3fe479c0e9939453983
JOSE SOSA <josea_1998@hotmail.com>
2023-01-29 19:09:21 -0500

    se cambia el archivo json por el objeto debido a que el json no estba siendo leido en producción

--- front/src/helpers/Anuncios/Anuncios.jsx
+++ front/src/helpers/Anuncios/Anuncios.jsx
@@ -5,10 +5,50 @@ import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { AnuncioItem } from './AnuncioItem';
+import dotenv from 'dotenv';
+
+
+
 
 export const Anuncios = () => {
 
     const [datosAnuncios, setDatosAnuncios] = useState([]);
+    
+    const anuncios= {
+        "anuncios": [
+            {
+                "id": 1,
+                "img": "https://i.postimg.cc/X7XyTLSx/img-anuncio-1.png",
+                "titulo": "Espectacular apartamento de lujo",
+                "descripcion": "Este impresionante apartamento de lujo cuenta con una ubicación privilegiada en el centro de la ciudad, con vistas panorámicas impresionantes y una amplia gama de servicios de lujo."
+            },
+            {
+                "id": 2,
+                "img": "https://i.postimg.cc/dVyZ000y/img-anuncio-2.png",
+                "titulo": "8000 metros cuadrados en Santa Solana",
+                "descripcion": "Atrae a compradores potenciales con una descripción por escrito clara que enumere las características más atractivas de tu propiedad."
+            },
+            {
+                "id": 3,
+                "img": "https://i.postimg.cc/VN9SjHns/img-anuncio-3.png",
+                "titulo": "Casa de campo con amplio jardín",
+                "descripcion": "Este encantador hogar de campo ofrece una gran cantidad de espacio al aire libre en su amplio jardín. Con una amplia sala de estar, cocina moderna y varias habitaciones, esta casa es ideal para una gran familia."
+            },
+            {
+                "id": 4,
+                "img": "https://i.postimg.cc/50xXpqSB/img-anuncio-4.png",
+                "titulo": "Apartamento de 2 dormitorios en Belden Park",
+                "descripcion": "Atrae a compradores potenciales con una descripción por escrito clara que enumere las características más atractivas de tu propiedad."
+            },
+            {
+                "id": 5,
+                "img": "https://i.postimg.cc/HL1c8CJP/img-anuncio-5.png",
+                "titulo": "Casa urbana clásica 3 dormitorios con aparcamiento y patio trasero",
+                "descripcion": "Atrae a compradores potenciales con una descripción por escrito clara que enumere las características más atractivas de tu propiedad."
+            }
+           
+        ]
+    };
 
     useEffect(() => {
         fetch('./src/json/anuncios.json')
@@ -31,8 +71,8 @@ export const Anuncios = () => {
             <section className='anuncios d-flex flex-column justify-content-center' id='anuncios'>
 
                 <Titulo children={"Anuncios"} />
-                {Array.isArray(datosAnuncios) && datosAnuncios.length > 0 &&
-                    datosAnuncios.map((anuncio, index) => (
+                {Array.isArray(anuncios.anuncios) && anuncios.anuncios.length > 0 &&
+                    anuncios.anuncios.map((anuncio, index) => (
                         <AnuncioItem key={index} anuncio={anuncio} />
                     ))
                 }
