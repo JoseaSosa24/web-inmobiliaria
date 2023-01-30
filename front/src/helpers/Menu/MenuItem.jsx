14fcde55369892dd6847ade767a15bd03f120e87
JOSE SOSA <josea_1998@hotmail.com>
2023-01-29 20:08:38 -0500

    agregando un manejo del dom para que desaparezca elmenu cuando seleccione la opción

--- front/src/helpers/Menu/MenuItem.jsx
+++ front/src/helpers/Menu/MenuItem.jsx
@@ -1,4 +1,9 @@
 export const MenuItem = ({ link, nombreItem, active, dropdown, children }) => {
+    const handleMenuItemClick = () => {
+        document.getElementById("navbarNavDropdown").classList.remove("show");
+      };
+    
+    
     return (
         // se crea un elemento 'li' con clase 'nav-item' y si 'dropdown' es verdadero, se agrega la clase 'dropdown'
         <li className={`nav-item ${dropdown ? 'dropdown' : ''}`}>
@@ -8,7 +13,7 @@ export const MenuItem = ({ link, nombreItem, active, dropdown, children }) => {
                 (
                     <>
                         {/*  // se crea un elemento 'a' con clases 'nav-link dropdown-toggle' y si 'active' es verdadero, se agrega la clase 'active' */}
-                        <a className={`nav-link dropdown-toggle ${active ? 'active' : ''}`} href={link} role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <a className={`nav-link dropdown-toggle ${active ? 'active' : ''}`} href={link} onClick={handleMenuItemClick} role="button" data-bs-toggle="dropdown" aria-expanded="false">
                             {/* // se muestra el nombre del item en el elemento 'a' */}
                             {nombreItem}
                         </a>
@@ -18,7 +23,7 @@ export const MenuItem = ({ link, nombreItem, active, dropdown, children }) => {
                 ) :
                 (
                     // se crea un elemento 'a' con clases 'nav-link' y si 'active' es verdadero, se agrega la clase 'active'
-                    <a className={`nav-link ${active ? 'active' : ''}`} aria-current="page" href={link}>
+                    <a className={`nav-link ${active ? 'active' : ''}`} onClick={handleMenuItemClick} aria-current="page" href={link}>
                         {/* // se muestra el nombre del item en el elemento 'a' */}
                         {nombreItem}
                     </a>
