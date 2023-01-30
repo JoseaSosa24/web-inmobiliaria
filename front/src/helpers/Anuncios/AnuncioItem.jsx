c920e01e8954012de72867b3e7d22e8e48d588ea
JOSE SOSA <josea_1998@hotmail.com>
2023-01-30 00:07:03 -0500

    se agregan signos a admiración

--- front/src/helpers/Anuncios/AnuncioItem.jsx
+++ front/src/helpers/Anuncios/AnuncioItem.jsx
@@ -7,7 +7,7 @@ export const AnuncioItem = ({ anuncio }) => {
             <div className="anuncio-info">
                 <h4 className="anuncio-title">{anuncio.titulo}</h4>
                 <p className="anuncio-description">{anuncio.descripcion}</p>
-                <a href="" className='anuncio-btn'>Consultanos ya</a>
+                <a href="" className='anuncio-btn'>¡Consultanos ya!</a>
             </div>
         </div>
     )
