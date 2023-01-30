99a636de6bec267b6dcc1ff7517a4bf251facfb0
JOSE SOSA <josea_1998@hotmail.com>
2023-01-29 20:08:10 -0500

    agregando una variable con la ruta de la img

--- front/src/helpers/Menu/Menu.jsx
+++ front/src/helpers/Menu/Menu.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './Menu.css'
 import { MenuLista } from './MenuLista';
-
+import logo from '/src/assets/icons/logo.png';
 const Menu = () => {
 
     const [scrolled, setScrolled] = useState(false);
@@ -21,7 +21,7 @@ const Menu = () => {
         <nav className={`navbar navbar-expand-lg bg-body-tertiary fixed-top `}>
             <section className="container-fluid ">
                 <a className="navbar-brand" href="#">
-                    <img className="logo" src="../../../src/assets/icons/logo.png" width={100} />
+                    <img className="logo" src={logo} width={100} />
                 </a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
