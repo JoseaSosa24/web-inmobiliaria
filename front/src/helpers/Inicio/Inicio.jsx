a080440fb3ca7140b7df0e5f9e2fce1ebd741098
JOSE SOSA <josea_1998@hotmail.com>
2023-01-30 02:15:28 -0500

    se agrega otra leyenda

--- front/src/helpers/Inicio/Inicio.jsx
+++ front/src/helpers/Inicio/Inicio.jsx
@@ -7,7 +7,8 @@ const Inicio = () => {
         <section className='inicio' id='inicio'>
             <img className="" src={imgInicio} />
             <div className="home-text-container">
-                <h1>Bienvenido a mi sitio</h1>
+                <h1>Haciendo realidad casas soñadas.</h1>
+                <h5>Tu sueño, nuestra realidad</h5>
             </div>
         </section>
     );
