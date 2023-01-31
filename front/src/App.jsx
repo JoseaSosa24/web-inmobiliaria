4dbc39418634b2d2d77b087ff44c187b9db40ae6
JOSE SOSA <josea_1998@hotmail.com>
2023-01-31 12:53:46 -0500

    Se crea boton de scrool-up

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Menu from './components/helpers/Menu/Menu'
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -8,12 +8,31 @@ import { Anuncios } from './components/helpers/Anuncios/Anuncios'
 import { Servicios } from './components/helpers/Servicios/Servicios'
 import { Contacto } from './components/helpers/Contacto/Contacto'
 import { Footer } from './components/helpers/Footer/Footer'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowUp } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [showBtn, setShowBtn] = useState(false);
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 100) {
+        setShowBtn(true);
+      } else {
+        setShowBtn(false);
+      }
+    });
+  }, []);
+
+
 
   return (
     <div className="App">
+      {showBtn && (
+        <button className='btn'>
+          <FontAwesomeIcon icon={faArrowUp} style={{ fontSize: '20px' }} onClick={() => window.scrollTo(0, 0)} />
+        </button>
+      )}
       <Menu />
       <Inicio />
       <Nosotros />
